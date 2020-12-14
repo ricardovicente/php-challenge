9fb22d42b5fd21d55c1c37fc6a670cd468ce0f61
Ricardo Vicente <listas@mediaworld.com.br>
2020-12-14 20:56:54 -0300

    Feature: emit to refresh table component

--- resources/views/livewire/file.blade.php
+++ resources/views/livewire/file.blade.php
@@ -30,7 +30,7 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
                     <div class="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg">
-                        <table class="min-w-full">
+                        <table wire:poll.5s class="min-w-full">
                             <thead>
                                 <tr>
                                     <th class="px-6 py-3 border-b-2 border-gray-100 text-left text-sm leading-4 text-blue-500 tracking-wider">Arquivo</th>
